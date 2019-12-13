ad0e37eb67a3ce6d65f15802dd83f55a88d34071
jak <jacob.kirkwood@bbconsult.co.uk>
2019-12-13 20:31:46 +0200

    add parser

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -8,48 +9,67 @@ namespace MyReport
     {
         static string command = @"log --pretty=tformat:""%s"" --since=""{0}"" --committer=""jak""";
         static string workingDirectory = @"C:\Projects\WPS";
+        static string url = @"https://pts.bbconsult.co.uk/taskEditor?id=";
+        static string descriptionPrefix = "\t\t - ";
 
 
         static void Main(string[] args)
         {
-            var result = GetGitLog();
-            var commits = result.Split("\n");
+            var log = GetGitLog();
+            var commits = log.Split("\n");
+            var result = ParseCommits(commits);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
 
+        private static List<string> ParseCommits(string[] commits)
+        {
             var lastNumber = "";
+            var result = new List<string>();
+
             foreach (var commit in commits)
             {
                 if (commit == "") continue;
                 var words = commit.Split(" ");
+                var decryption = descriptionPrefix +
+                                 commit.Substring(words[0].Length + 1, commit.Length - words[0].Length - 1);
+
                 if (lastNumber != words[0])
                 {
-                    Console.WriteLine("https://pts.bbconsult.co.uk/taskEditor?id=" + words[0]);
-                    Console.WriteLine(commit);
+                    result.Add(url + words[0]);
+                    result.Add(decryption);
                     lastNumber = words[0];
                 }
-                else Console.WriteLine(commit);
+                else result.Add(decryption);
             }
+
+            return result;
         }
 
-        public static string GetGitLog()
+        private static string GetGitLog()
         {
-            var yestoday = DateTime.Now.AddDays(-1).ToString("MM/dd/yyyy");
-            var output = RunProcess(string.Format(command, yestoday));
+            var yesterday = DateTime.Now.AddDays(-1).ToString("MM/dd/yyyy");
+            var output = RunProcess(string.Format(command, yesterday));
             return output;
         }
 
         private static string RunProcess(string command)
         {
-            // Start the child process.
-            Process p = new Process();
-            // Redirect the output stream of the child process.
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.FileName = "git.exe";
-            p.StartInfo.Arguments = command;
-            p.StartInfo.WorkingDirectory = workingDirectory;
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    FileName = "git.exe",
+                    Arguments = command,
+                    WorkingDirectory = workingDirectory
+                }
+            };
             p.Start();
-            // Read the output stream first and then wait.
-            string output = p.StandardOutput.ReadToEnd();
+            var output = p.StandardOutput.ReadToEnd();
             p.WaitForExit();
             return output;
         }
