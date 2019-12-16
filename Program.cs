3e08cf9ea29ae30b499a3ae503529cfee98ef12c
Jacob Kirkwood <jacob.kirkwood@bbconsult.co.uk>
2019-12-16 12:50:28 +0200

    settings

--- Program.cs
+++ Program.cs
@@ -5,40 +5,52 @@ using System.IO;
 
 namespace MyReport
 {
-    class Program
+    static class Program
     {
-        static string command = @"log --pretty=tformat:""%s"" --since=""{0}"" --committer=""jak""";
-        static string workingDirectory = @"C:\Projects\WPS";
-        static string url = @"https://pts.bbconsult.co.uk/taskEditor?id=";
-        static string descriptionPrefix = "\t\t - ";
-
+        private const string Command = @"log --pretty=tformat:""%s"" --since=""{0}"" --committer=""{1}""";
+        private const string NameCommiter = @"Jacob Kirkwood";
+        private const string WorkingDirectory = @"C:\Projects\WPS";
+        private const string Url = @"https://pts.bbconsult.co.uk/taskEditor?id=";
+        private const string DescriptionTitlePrefix = "\t - ";
+        private const string DescriptionPrefix = "\t\t - ";
+        private const string GitPath = "git.exe";
+        private const string FormatDate = "dd/MM/yyyy";
+        private const string ReportPath = @"C:\Users\Admin\Desktop\Reports.txt";
 
         static void Main(string[] args)
         {
             var log = GetGitLog();
             var commits = log.Split("\n");
             var result = ParseCommits(commits);
+            WriteToFile(result);
+        }
+
+        private static void WriteToFile(List<string> result)
+        {
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(ReportPath, true);
             foreach (var item in result)
             {
+                writer.WriteLine(item);
                 Console.WriteLine(item);
             }
+
+            writer.Close();
         }
 
         private static List<string> ParseCommits(string[] commits)
         {
             var lastNumber = "";
-            var result = new List<string>();
+            var result = new List<string> {DateTime.Now.ToString(FormatDate)};
 
             foreach (var commit in commits)
             {
                 if (commit == "") continue;
-                var words = commit.Split(" ");
-                var decryption = descriptionPrefix +
+                var words = commit.Split(@" ");
+                var decryption = DescriptionPrefix +
                                  commit.Substring(words[0].Length + 1, commit.Length - words[0].Length - 1);
-
                 if (lastNumber != words[0])
                 {
-                    result.Add(url + words[0]);
+                    result.Add(DescriptionTitlePrefix + Url + words[0]);
                     result.Add(decryption);
                     lastNumber = words[0];
                 }
@@ -50,8 +62,8 @@ namespace MyReport
 
         private static string GetGitLog()
         {
-            var yesterday = DateTime.Now.AddDays(-1).ToString("MM/dd/yyyy");
-            var output = RunProcess(string.Format(command, yesterday));
+            var yesterday = DateTime.Now.AddDays(-1).ToString(FormatDate);
+            var output = RunProcess(string.Format(Command, yesterday, NameCommiter));
             return output;
         }
 
@@ -63,9 +75,9 @@ namespace MyReport
                 {
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
-                    FileName = "git.exe",
+                    FileName = GitPath,
                     Arguments = command,
-                    WorkingDirectory = workingDirectory
+                    WorkingDirectory = WorkingDirectory
                 }
             };
             p.Start();
